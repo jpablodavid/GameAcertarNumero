0e07411570c80af4ac41da2458ed472ed69876f5
jpablodavid <jpablodavid1307@gmail.com>
2021-09-16 13:11:54 -0300

    Inicio

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
+export default function App() { 
+  const inputAccessoryViewID = "uniqueID";
+	const initialText = "Placeholder Text";
+	const [text, setText] = useState(initialText);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<Text></Text>
+			<TextInput
+				style={{
+					padding: 16,
+					marginTop: 50,
+				}}
+				inputAccessoryViewID={inputAccessoryViewID}
+				onChangeText={(text) => setText(text)}
+				value={text}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
